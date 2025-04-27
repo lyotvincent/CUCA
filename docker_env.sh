682ee5568e4caed88516c30450eb8ecb8a94d852
gatsby2016 <gatsby2015@163.com>
2025-04-27 10:37:38 +0000

    udpate docker container run

--- docker_env.sh
+++ docker_env.sh
@@ -1,11 +1,15 @@
+projname=CUCA
+abspath=/home/cyyan/Projects/
+imagename=crpi-vgk1sxg5ba2a8sff.cn-hangzhou.personal.cr.aliyuncs.com/cuca_proj_env/cuca_env
+
 docker run -dit \
     --name cuca_proj_env \
     --gpus all \
     --cpus 64 \
-    --mount type=bind,source=/your_proj_absolute_path/CUCA/,target=/home/appuser/CUCA \
+    --mount type=bind,source=${abspath}${projname},target=/home/appuser/${projname} \
     --shm-size 128g \
     -p 6009:6009 \
-    cuca_env:v1_0 \
+    ${imagename}:v1_0 \
     /bin/bash -c 'while true; do echo `date`; sleep 600; done'
 
 echo "docker exec -it cuca_proj_env /bin/bash"
